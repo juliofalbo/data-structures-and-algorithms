655fd73adcedb2de40c9e5a53a98ad8763656b4c
Júlio Falbo <julio.falbo.rj@gmail.com>
2018-12-18 18:47:06 -0200

    first commit

---
+++ src/main/java/datastructure/linkedlist/LinkedList.java
@@ -0,0 +1,119 @@
+package datastructure.linkedlist;
+
+public class LinkedList<T> {
+
+    private static class Node<T> {
+
+        T data;
+
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+
+    public void addFront(final T data) {
+        Node<T> newNode = new Node<>(data);
+
+        if (headIsNull()) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = this.head;
+        head = newNode;
+    }
+
+    public T getFirst() {
+        validateHead();
+        return (T) head.data;
+    }
+
+    public T getLast() {
+        return (T) findTail().data;
+    }
+
+    public void addBack(final T data) {
+        if(headIsNull()){
+            addFront(data);
+            return;
+        }
+        findTail().next = new Node<>(data);
+    }
+
+    public int size() {
+        if(headIsNull()){
+            return 0;
+        }
+        int count = 1;
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+            count++;
+        }
+        return count;
+    }
+
+    public boolean contains(T data){
+        Node current = head;
+        if(current.data.equals(data)){
+            return true;
+        }
+        while (current.next != null) {
+            if(current.next.data.equals(data)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void clear() {
+        head = null;
+    }
+
+    public void removeFront() {
+        head = head.next;
+    }
+
+    public void removeBack() {
+        head = head.next;
+    }
+
+    public void deleteValue(final T data) {
+        validateHead();
+        if(head.data.equals(data)){
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if(current.next.data.equals(data)){
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    private void validateHead() {
+        if(headIsNull()){
+            throw new IllegalStateException("The list is empty");
+        }
+    }
+
+    private Node findTail() {
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private boolean headIsNull() {
+        return head == null;
+    }
+}
---
+++ src/test/java/datastructure/linkedlist/LinkedListTest.java
@@ -0,0 +1,110 @@
+package datastructure.linkedlist;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+    private LinkedList<Integer> linkedList;
+
+    @Before
+    public void setUp() throws Exception {
+        linkedList = new LinkedList<>();
+    }
+
+    @Test
+    public void addFront() {
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        Assert.assertEquals(3, (int) linkedList.getFirst());
+        Assert.assertEquals(1, (int) linkedList.getLast());
+    }
+
+    @Test
+    public void setFirst() {
+        linkedList.addFront(1);
+        Assert.assertEquals(1, (int) linkedList.getFirst());
+    }
+
+    @Test
+    public void setLast() {
+        linkedList.addFront(1);
+        linkedList.addFront(2);
+        linkedList.addFront(3);
+
+        Assert.assertEquals(1, (int) linkedList.getLast());
+    }
+
+    @Test
+    public void addBack() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        Assert.assertEquals(1, (int) linkedList.getFirst());
+        Assert.assertEquals(3, (int) linkedList.getLast());
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(0, linkedList.size());
+        linkedList.addBack(1);
+        Assert.assertEquals(1, linkedList.size());
+        linkedList.addBack(2);
+        Assert.assertEquals(2, linkedList.size());
+    }
+
+    @Test
+    public void clear() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        linkedList.clear();
+
+        Assert.assertEquals(0, linkedList.size());
+    }
+
+    @Test
+    public void contains() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        Assert.assertEquals(true, linkedList.contains(1));
+        Assert.assertEquals(true, linkedList.contains(2));
+        Assert.assertEquals(true, linkedList.contains(3));
+        Assert.assertEquals(false, linkedList.contains(4));
+    }
+
+    @Test
+    public void deleteValue() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        linkedList.deleteValue(2);
+
+        Assert.assertEquals(2, linkedList.size());
+        Assert.assertEquals(1, (int) linkedList.getFirst());
+        Assert.assertEquals(3, (int) linkedList.getLast());
+    }
+
+    @Test
+    public void removeFront() {
+        linkedList.addBack(1);
+        linkedList.addBack(2);
+        linkedList.addBack(3);
+
+        linkedList.removeFront();
+        Assert.assertEquals(2, (int) linkedList.getFirst());
+
+        linkedList.removeFront();
+        Assert.assertEquals(3, (int) linkedList.getFirst());
+    }
+
+    // Remove back
+}
